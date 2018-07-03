794b8f27751e8ab795784b79afcb518ad0c77be9
ddgilli <35736012+ddgilli@users.noreply.github.com>
2018-07-03 08:16:35 -0400

    still debugging

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -16,13 +16,12 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" component={LoginPage} exact={true}/>
-        <Route path="/dashboard" component={ExpenseDashboardPage}/>
-        <Route path="/create" component={AddExpensePage}/>
-        <Route path="/edit/:id" component={EditExpensePage}/>
-        <Route path="/about" component={AboutPage}/>
-        <Route component={NotFoundPage}/>
-      </Switch>
+      <PublicRoute path="/" component={LoginPage} exact={true} />
+      <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+      <PrivateRoute path="/create" component={AddExpensePage} />
+      <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+      <Route component={NotFoundPage} />
+    </Switch>
     </div>
   </Router>
   );
--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -7,21 +7,21 @@ import { Route, Redirect } from 'react-router-dom';
 * to add conditional logic to our navigation.
 */
 export const PublicRoute = ({
-  isAuthenticated, 
+  isAuthenticated,
   component: Component,
-  ...rest //gives us access to the rest of props - can call it anything
+  ...rest
 }) => (
-  <Route {...rest} component={(props) => (
-    isAuthenticated ? (
-      <Redirect to="/dashboard" /> //not authenticated, send to login page
+    <Route {...rest} component={(props) => (
+      isAuthenticated ? (
+        <Redirect to="/dashboard" />
       ) : (
-        <Component {...props} /> 
-      )
-  )}/>
-);
+          <Component {...props} />
+        )
+    )} />
+  );
 
 const mapStateToProps = (state) => ({
-  isAuthenticated: !!state.auth.uid //now returns true or false
+  isAuthenticated: !!state.auth.uid
 });
 
 export default connect(mapStateToProps)(PublicRoute);
